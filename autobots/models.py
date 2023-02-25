86bdd75b63ae4d0f864f8bf5b9757df85bc678c4
mabeijing <58149278@qq.com>
2023-02-25 12:13:10 +0800

    add models.py

--- autobots/models.py
+++ autobots/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Question(models.Model):
+    content = models.CharField(max_length=100)
+
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=10)
